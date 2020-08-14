0533c71de74832ff59e524fe49c73dd568652816
juanjosearriaza <juanjosearriaza@hotmail.com>
2020-08-14 13:23:16 +0100

    add css to comments

--- gofullstack/worknetwork/src/components/Comment.vue
+++ gofullstack/worknetwork/src/components/Comment.vue
@@ -1,24 +1,17 @@
 <template>
   <div class="card mb-3" style="width:100%">
-    <b-list-group-item class="d-flex align-items-center">
-        <b-avatar class="mr-3"></b-avatar>
-        <span class="mr-auto">{{ user.firstname }}   {{ user.lastname }} </span>
-        <b-badge>Posted {{ createdAt }}</b-badge>
-      </b-list-group-item>
-    <div class="card-body">
-      <p class="card-text text-left">{{ comment }} </p>
-      
-
-  
-      
-    </div>
+    <b-list-group-item class="d-flex align-items-left">
+      <b-avatar class="mr-2"></b-avatar>
+      <div id="comment">
+        <h6>{{ user.firstname }} {{ user.lastname }}</h6>
+        <div class="card-text text-left">{{ comment }}</div>
+      </div>
+    </b-list-group-item>
+    <p>Posted {{ createdAt }}</p>
   </div>
 </template>
 
 <script>
-
-
-
 export default {
   name: "Comment",
   props: ["id", "comment", "userId", "postId", "createdAt"],
@@ -26,12 +19,37 @@ export default {
     user() {
       const users = this.$store.getters.allUsers;
 
-      return users.find((user) => this.userId == user.id) || {firstname: null, lastname: null};
+      return (
+        users.find((user) => this.userId == user.id) || {
+          firstname: null,
+          lastname: null,
+        }
+      );
     },
-    
-  }
+  },
 };
 </script>
 
 <style scoped>
+.card {
+  border-style: none !important;
+}
+#comment {
+  width: 100%;
+  text-align: left;
+  margin-left: auto;
+  background-color: rgba(0, 0, 0, 0.1);
+  border-radius: 5px;
+  padding: 5px 5px;
+}
+h6 {
+  font-weight: bolder;
+}
+.list-group-item {
+   border-style: none !important;
+}
+p {
+    text-align: left;
+    margin-left: 65px;
+}
 </style>
