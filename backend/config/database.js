e7f99f34fa890651a3aa414365be568048c60f94
juanjosearriaza <juanjosearriaza@hotmail.com>
2020-08-28 14:58:22 +0100

    add files

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,10 +1,5 @@
 const Sequelize = require('sequelize');
 
-/*module.exports = new Sequelize('worknetwork', 'root', '1234', {
-    host: 'localhost',
-    dialect: 'mysql',
-    
-  });*/
 
   module.exports = new Sequelize('worknetwork', 'postgres', '1234', {
     host: 'localhost',
